d600cba1f90303df3196a0347d7b666b1cd24b98
Erik Töyrä <erik.toyra@stendahls.se>
2018-10-09 22:29:05 +0200

    Injects "tailwindcss" to invoking project's devDependenies.

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,14 @@
 module.exports = (api, options) => {
-  api.injectImports(api.entryFile, `import './assets/css/tailwind.css'`);
+  // Modify package.json fields
+  api.extendPackage({
+    devDependencies: {
+      "tailwindcss": "^0.6.6",
+    }
+  })
+
+  // Inject Tailwind into the main.js file
+  api.injectImports(api.entryFile, `import '@/assets/css/tailwind.css'`);
+
+  // Copy and render all files in ./template
   api.render('./template')
 }
